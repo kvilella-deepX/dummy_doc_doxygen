7065e739d93958a676ff50887f2114a2e1a92d9b
kvilella-deepX <kvilella-deepX@users.noreply.github.com>
2022-09-08 14:40:07 +0900

    aaa

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -26,6 +26,24 @@ html_sidebars = [
     "versioning.html",
 ]
 
+# Whitelist pattern for tags (set to None to ignore all tags)
+smv_tag_whitelist = r'^.*$'
+
+# Whitelist pattern for branches (set to None to ignore all branches)
+smv_branch_whitelist = None
+
+# Whitelist pattern for remotes (set to None to use local branches only)
+smv_remote_whitelist = None
+
+# Pattern for released versions
+smv_released_pattern = r'^tags/.*$'
+
+# Format for versioned output directories inside the build directory
+smv_outputdir_format = 'version/{config.release}'
+
+# Determines whether remote or local git branches/tags are preferred if their output dirs conflict
+smv_prefer_remote_refs = False
+
 import subprocess
 subprocess.call('make clean', shell=True)
 subprocess.call('cd ../../doxygen ; doxygen', shell=True)
