0603ffd6d6ce65356529ae2092b7757d8ce94bc2
kvilella-deepX <kvilella-deepX@users.noreply.github.com>
2022-09-08 11:51:06 +0900

    aaa

---
+++ docs/sphinx/source/conf.py
@@ -0,0 +1,25 @@
+project = 'aaa'
+copyright = '2022'
+author = 'KV'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.imgmath', 
+    'sphinx.ext.todo',
+    'breathe',
+]
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'furo' # 'alabaster'
+
+import subprocess
+subprocess.call('make clean', shell=True)
+subprocess.call('cd ../../doxygen ; doxygen', shell=True)
+
+breathe_projects = { "aaa": "../../doxygen/build/xml/" }
+breathe_default_project = "aaa"
